0709faabe182da65f8d8438e18ca08df167ec532
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-06-27 18:01:59 -0600

    FEAT: Guardado de respaldo Api

--- SiteManager.Domain/obj/Debug/net10.0/SiteManager.Domain.AssemblyInfo.cs
+++ SiteManager.Domain/obj/Debug/net10.0/SiteManager.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteManager.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9cd1d979af3a3fbdc8c6d6f9176895e8d9fb8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7efdfedf83f14d023e9d33d0107368684a53c426")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteManager.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteManager.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
